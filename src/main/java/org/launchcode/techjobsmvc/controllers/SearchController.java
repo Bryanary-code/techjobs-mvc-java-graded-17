b130d306d3b136ece19f41579eee86c3116c4acf
Bryan Wulfert <bwulfert@gmail.com>
2024-01-27 20:19:51 -0600

    all tasks completed test

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,5 +1,7 @@
 package org.launchcode.techjobsmvc.controllers;
 
+import org.launchcode.techjobsmvc.models.Job;
+import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
@@ -27,10 +30,21 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
     @PostMapping("results")
-    public String displaySearchResults(Model model, @RequestParam String searchTerm, HashMap<String, String> choices) {
-        model.addAttribute("columns", choices);
-        model.addAttribute("searchTerm", searchTerm);
-        return "search/results";
+    public String displaySearchResults(Model model, @RequestParam String searchTerm, @RequestParam String choices) {
+        ArrayList<Job> jobs;
+
+        if ("all".equals(searchTerm) || searchTerm.isEmpty()) {
+            // If the user enters "all" or leaves the box empty, call findAll() method
+            jobs = JobData.findAll();
+        } else {
+            // Otherwise, send the search information to findByColumnAndValue
+            jobs = JobData.findByColumnAndValue(choices, searchTerm);
+        }
+
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("columns", ListController.columnChoices);
+
+        return "search";
     }
 
 }
