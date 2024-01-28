b5898a5fed03e8fec6f095d7919f65b6e449ac39
Bryan Wulfert <bwulfert@gmail.com>
2024-01-27 19:33:38 -0600

    completed all jobs list view

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -3,7 +3,11 @@ package org.launchcode.techjobsmvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
 
@@ -22,6 +26,12 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String searchTerm, HashMap<String, String> choices) {
+        model.addAttribute("columns", choices);
+        model.addAttribute("searchTerm", searchTerm);
+        return "search/results";
+    }
 
 }
 
